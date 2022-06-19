e217d8131264ab55577c19d0f35797926b0c7b5f
Rukshan Perera <pererarukshan45@gmail.com>
2022-06-20 00:17:05 +0530

    add new js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -100,13 +100,14 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
+
          chosenCategoryShortName ="'"+chosenCategoryShortName+"'";
          var homeHtmlToInsertIntoMainPage=insertProperty(homeHtml, "short_name",chosenCategoryShortName );
-
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
+      
          insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
     },
